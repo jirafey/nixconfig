6c085ed299f53c011bc683bac49e78ee70a1c3fe
jirafey <jirafey@tuta.io>
2024-07-23 01:08:46 +0200

    Update home.nix

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -1,17 +1,23 @@
 # home.nix
-{ lib, pkgs, ... }:
-{
-  home = {
-    packages = with pkgs; [
-      hello
-    ];
-
-    # This needs to actually be set to your username
-    username = "user";
-    homeDirectory = "/home/user";
-
+{ config, pkgs, ... }:
+  {
+      
+    home.username = "user";
+    home.homeDirectory = "/home/user";
     # You do not need to change this if you're reading this in the future.
     # Don't ever change this after the first build.  Don't ask questions.
-    stateVersion = "24.05";
+    home.stateVersion = "24.05";
+
+    programs.home-manager.enable = true;
+    
+   programs.firefox = {
+  enable = true;
+  profiles = {
+    dev-edition-default = {
+      userChrome = ''
+        /* some css */
+      '';
+    };
   };
-}
+};
+  }
