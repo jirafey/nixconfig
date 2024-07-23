7f67ade912deb75db786b725361a5b3a4e218129
jirafey <jirafey@tuta.io>
2024-07-23 16:12:38 +0200

    Update home.nix

--- etc/nixos/home.nix
+++ etc/nixos/home.nix
@@ -1,23 +1,22 @@
 # home.nix
-{ config, pkgs, ... }:
-  {
-      
-    home.username = "user";
-    home.homeDirectory = "/home/user";
-    # You do not need to change this if you're reading this in the future.
-    # Don't ever change this after the first build.  Don't ask questions.
-    home.stateVersion = "24.05";
+{ lib, pkgs, ... }:
+{
+  home.username = "user";
+  home.homeDirectory = "/home/user";
+  # You do not need to change this if you're reading this in the future.
+  # Don't ever change this after the first build.  Don't ask questions.
+  home.stateVersion = "24.05";
 
-    programs.home-manager.enable = true;
-    
-   programs.firefox = {
-  enable = true;
-  profiles = {
-    dev-edition-default = {
-      userChrome = ''
-        /* some css */
-      '';
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      userChrome = builtins.readFile ./userChrome.css;
+      userContent = builtins.readFile ./userContent.css;
+      settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+	"extensions.pocket.enabled" = false;
+	"browser.toolbars.bookmarks.visibility" = "never";
+        };
+      };
     };
-  };
-};
-  }
+}
