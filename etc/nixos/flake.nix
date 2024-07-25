46b723697c3c9e65266c3350a0a4a94782d84077
jirafey <jirafey@tuta.io>
2024-07-25 19:55:02 +0000

    Update flake.nix

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     # NixOS official package source, using the nixos-24.05 branch here
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager?ref=release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -21,7 +21,12 @@
 	home-manager.nixosModules.home-manager {
 	home-manager.useGlobalPkgs = true;
 	home-manager.useUserPackages = true;
-	home-manager.users.user = import ./home.nix;
+	home-manager.users.user = {
+	  imports = [
+	./home.nix
+	./firefox.nix
+	];
+	};
 	}
       ];
     };
