a44f90c4b014378e3b8b3e7306a3eceb87c6ed1d
jirafey <jirafey@tuta.io>
2024-07-25 15:02:33 +0000

    Update flake.nix

--- etc/nixos/flake.nix
+++ etc/nixos/flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     # NixOS official package source, using the nixos-24.05 branch here
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.05";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    home-manager.url = "github:nix-community/home-manager?ref=release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
