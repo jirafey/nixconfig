102fa28ff49b8e820e1d768de635e498343874a5
jirafey <jirafey@tuta.io>
2024-08-04 15:19:30 +0000

    Update home.nix

--- home.nix
+++ home.nix
@@ -6,7 +6,7 @@
   imports = [
     # inputs.nixvim.homeManagerModules.nixvim
     ];
-
+  nixpkgs.overlays = [ <kickstart-nix-nvim>.overlays.default ];
 
   home.username = "user";
   home.homeDirectory = "/home/user";
@@ -14,6 +14,7 @@
   # Don't ever change this after the first build.  Don't ask questions.
   home.stateVersion = "24.05";
   home.packages = [
-    inputs.nvim-flake.packages.${pkgs.stdenv.system}.default
+    #inputs.nvim-flake.packages.${pkgs.stdenv.system}.default
+    inputs.kickstart-nix.packages.${pkgs.stdenv.system}.default
   ];
 }
